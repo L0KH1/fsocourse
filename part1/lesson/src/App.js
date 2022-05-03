3c9b152c94bc238fc953aef240ab2b6921e6972d
allil22@mail.wlu.edu <43547464+L0KH1@users.noreply.github.com>
2022-05-03 07:10:17 -0400

    cleaning up duplicates in lesson

--- part1/lesson/src/App.js
+++ part1/lesson/src/App.js
@@ -1,20 +1,47 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>'Ello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
+import { useState } from 'react'
+
+const History = (props) => {  
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+      )
+    }
+    return (
+      <div>
+        button press history: {props.allClicks.join(' ')}
+      </div>
+    )
 }
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
-  const name = "Lucy"
-  const age = 22
-  return(
-  <div>
-    <h1>Greetings</h1>
-    <Hello name="Luke" age={22}/>
-    <Hello name={name} age={age}/>
-  </div>
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
   )
 }
 
